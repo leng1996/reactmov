6aee6bb1b07db7c6c0bcf4c12ba3c27f0660f9c5
leng1996 <1776350459@qq.com>
2019-08-29 17:07:11 +0800

    提交react-project

---
+++ public/precache-manifest.2a1e00f1d29d8d2384601667c5935693.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ced7269442ba3e752512b16a796689ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ccb35d2e0ceef7f5607c",
+    "url": "/static/css/main.7289f0df.chunk.css"
+  },
+  {
+    "revision": "4e28ab4bd88eb2748514",
+    "url": "/static/js/2.29b57fdb.chunk.js"
+  },
+  {
+    "revision": "ccb35d2e0ceef7f5607c",
+    "url": "/static/js/main.99089e4e.chunk.js"
+  },
+  {
+    "revision": "f60f47b4967be61cb750",
+    "url": "/static/js/runtime~main.f3d926c4.js"
+  }
+]);
